5de23975cba3a65e01869744f5f6f23a77eac534
W3ndig0 <70271139+W3ndig0u0@users.noreply.github.com>
2022-08-31 14:37:12 +0200

    TOp G

--- Lektion-1/print.py
+++ Lektion-1/print.py
@@ -1,10 +1,48 @@
+
+# !Impoterar Biblotek
+import random
+import collections
+
+# !Definerar varibler
 welcome = "välkommmen"
 name = "vad är ditt namn"
-ques = "Vilka två siffror vill du addera"
-nameAnswer = None
+ques = "Vilka två heltal vill du slumpa en siffra mellan"
 ques2 = "Hejsan,"
 
+nameAnswer = None
+firstNum = None
+secNum = None
+
+
+# !Funktioner
+
+# ?Borde ha en funktion för att skriva ett tal allmänt istället för en som tar 2,
+# ?Det blir mer flexibel och lättare då.
+
+def chooseNumber(num1, num2):
+    numList = []
+    num1 = int(input("Vad är det första talet du väljer? "))
+    num2 = int(input("Vad är det andra talet du väljer? "))
+    numList.insert(num1, num2)
+    return numList
+
+
+# !Logik
 print(welcome)
 print(name)
+
 nameAnswer = input()
+
 print(ques2, nameAnswer)
+print(ques)
+
+# chooseNumber(firstNum, secNum)
+print(chooseNumber(firstNum, secNum))
+
+# if firstNum >= secNum:
+# print("Din första tal måste vara mindre än den andra talet")
+
+
+randomNumber = random.randint(firstNum, secNum)
+
+print(randomNumber)
